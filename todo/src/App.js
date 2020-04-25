106ff6593fee6977aea994158f747a7c33dd8a5b
abena1apaw <57848351+abena1apaw@users.noreply.github.com>
2020-04-24 23:50:48 -0500

    added new app

---
+++ todo/src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+constructor(props) {
+super(props);
+
+this.state={
+newItem:"",
+list:[]
+}
+}
+
+updateInput(key, value){
+
+this.setState({
+[key]: value
+})
+}
+addItem() {
+const newItem={id: 1 + Math.random(),
+value: this.state.newItem.slice()
+};
+
+const list = [...this.state.list];
+
+list.push(newItem);
+this.setState({
+list,
+newItem:""
+});
+} 
+deleteItem(id){
+const list = [...this.state.list];
+const updatedList = list.filter(item => item.id !== id);
+this.setState({list: updatedList})
+}
+
+render() {
+  return (  
+    <div className ="App">
+    <div>
+        Add an item...
+          <br/>
+<input type="text"
+placeholder="type to do here..."
+value={this.state.newItem}
+onchange={e => this.updateInput("newItem", e.target.value)}
+/>
+<button
+onclick={() => this.addItem()}
+>
+Add
+</button>
+<br/>
+<ul>
+{this.state.list.map(item => {
+return (
+<li key={item.id}>
+{item.value}
+<button
+onClick={() => this.deleteItem(item.id)}
+>
+X
+</button>
+</li>
+)}
+)}
+</ul>
+</div>
+</div>
+  );
+}}
+export default App;
